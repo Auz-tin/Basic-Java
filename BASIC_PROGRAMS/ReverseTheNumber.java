f3435e57f4e63518f511937198e61d580973c624
Auz-tin <74496000+Auz-tin@users.noreply.github.com>
2020-11-16 22:08:02 +0530

    Update ReverseTheNumber.java

--- BASIC_PROGRAMS/ReverseTheNumber.java
+++ BASIC_PROGRAMS/ReverseTheNumber.java
@@ -1,17 +1,17 @@
 package BASIC_PROGRAMS;
-
 import java.util.Scanner;
+
+// Reverse the given number
 
 public class ReverseTheNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner s=new Scanner(System.in);
 		
 		System.out.println("Enter the number: ");
 		int a=s.nextInt();
 		
-		int b=a;
 		int sum=0;
 		while(a>0) {
 			int rem=a%10;
