f11612d6aa5c15160cb3144ecd38ad2ed62dfb02
Auz-tin <74496000+Auz-tin@users.noreply.github.com>
2020-11-16 12:59:47 +0530

    Update If_else.java

--- basics/If_else.java
+++ basics/If_else.java
@@ -7,7 +7,7 @@ public class If_else {
 		int b=10;
 		
 		if(a>b) {
-			System.out.println("A1 is greater");
+			System.out.println("A is greater");
 		}
 		
 		else if(b>a){
@@ -15,6 +15,22 @@ public class If_else {
 		}	
 		else {
 			System.out.println("Both are equal");
+		}
+		
+		int x=10, y=15, z=5;
+		
+		if(x>y && x>z){
+			System.out.println("x is greater");
+		}
+		
+		else if(y>x && y>z){
+			System.out.println("y is greater");
+		}
+		else if(z>x && z>y){
+			System.out.println("z is greater");
+		}
+		else {
+			System.out.println("All are equal");
 		}
   }
 }
