f882b0c7dd180b572534953ae7911d7e3e0e4e48
Auz-tin <74496000+Auz-tin@users.noreply.github.com>
2020-11-16 10:55:49 +0530

    Update Loops_1.java

--- basics/Loops_1.java
+++ basics/Loops_1.java
@@ -3,21 +3,20 @@ public class Loops_1 {
     // breaks and continue
     public static void main(String[] args) {
 		
-	  for(int i=0;i<5;i++) {
-      if(i==2){
-        break;  // cut the entire loop when i=2
-      }
-		  System.out.println(i);
-	  }
+	for(int i=0;i<5;i++) {
+      		if(i==2){
+        	break;  // cut the entire loop when i=2
+      		}
+		System.out.println(i);
+	}
     
-     for(int i=0;i<5;i++) {
-      if(i==2){
-        continue;  
-        // cut the particular iteration (i=2), skip code after this line it goes to next iteration (i=3)
-      }
-		  System.out.println(i);
-	  }
-      
+     	for(int i=0;i<5;i++) {
+      		if(i==2){
+        	continue;  
+        	// cut the particular iteration (i=2), skip code after this line it goes to next iteration (i=3)
+      		}
+		System.out.println(i);
+	}
     	
    }
 }
