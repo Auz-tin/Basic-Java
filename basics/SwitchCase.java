97465dca55f06c03b0db4a83ba2f216894080caa
Auz-tin <74496000+Auz-tin@users.noreply.github.com>
2020-11-16 12:46:50 +0530

    Update SwitchCase.java

--- basics/SwitchCase.java
+++ basics/SwitchCase.java
@@ -5,7 +5,7 @@ public class SwitchCase {
 	public static void main(String[] args) {
     
 		int x=4;
-		System.out.println("x: "+x);
+		
 		switch(x) {
 			case 0:
 				System.out.println("zero");
@@ -19,6 +19,41 @@ public class SwitchCase {
 			default:
 				System.out.println("more than two");
 				break;
+		}
+		
+		char grade='a';
+		
+		switch(grade) {
+			case 'a':
+				System.out.println("Excellent");
+				break;	
+			case 'b':
+				System.out.println("Good");
+				break;
+			case 'c':
+				System.out.println("Average");
+				break;
+			default:
+				System.out.println("fail");
+				break;
+		}
+		
+		
+		String remarks = "Excellent";
+
+		switch (remarks) {
+		case "Excellent":
+			System.out.println("A grade");
+			break;
+		case "Good":
+			System.out.println("B grade");
+			break;
+		case "Average":
+			System.out.println("C grade");
+			break;
+		default:
+			System.out.println("fail");
+			break;
 		}
 		
 	}
