8af9a891a2cbec557a878666a6fe0ef5fe8f3126
Auz-tin <74496000+Auz-tin@users.noreply.github.com>
2020-11-16 10:43:28 +0530

    Update Loops.java

--- basics/Loops.java
+++ basics/Loops.java
@@ -1,25 +1,25 @@
 public class Loops {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		
-    // for loop
-		for(int i=0;i<5;i++) {
-			System.out.println(i);
-		}
-    
-    // while loop
-    int j=0;
-    while(j<10){
-      System.out.println(j);
-      j++;
-    }
-		
-    // do-while loop
-		int i=0;
-		do {
-			System.out.println("i: "+i);
-			i++;
-		}while(i>1);
+    	// for loop
+	for(int i=0;i<5;i++) {
+		System.out.println(i);
 	}
+    
+    	// while loop
+    	int j=0;
+    	while(j<10){
+    		System.out.println(j);
+      		j++;
+    	}
+		
+    	// do-while loop
+	int i=0;
+	do {
+		System.out.println("i: "+i);
+		i++;
+	}while(i>1);
+   }
 
 }
