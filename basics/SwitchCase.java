9d3cbbe67718a13ef7b179ce03b518ba8c51ad3a
Auz-tin <74496000+Auz-tin@users.noreply.github.com>
2020-11-16 12:38:00 +0530

    Update SwitchCase.java

--- basics/SwitchCase.java
+++ basics/SwitchCase.java
@@ -1,1 +1,27 @@
+package basics;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+    
+		int x=4;
+		System.out.println("x: "+x);
+		switch(x) {
+			case 0:
+			case 1:
+				System.out.println("Binary");
+				break;
+			case 2:
+				System.out.println("2");
+				break;
+			case 3:
+				System.out.println("3");
+			default:
+				System.out.println("default");
+				break;
+		}
+		
+	}
+
+}
 
