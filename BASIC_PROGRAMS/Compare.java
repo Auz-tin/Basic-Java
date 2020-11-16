e75e2d4bfe180c072ac7a4ceff6c5b6b3aef7aea
Auz-tin <74496000+Auz-tin@users.noreply.github.com>
2020-11-16 22:03:19 +0530

    Update Compare.java

--- BASIC_PROGRAMS/Compare.java
+++ BASIC_PROGRAMS/Compare.java
@@ -1,22 +1,20 @@
 package BASIC_PROGRAMS;
 
+// compare two strings and find number of occurence of String s1 in String s
+
 public class Compare {
 
 	public static void main(String[] args) {
 		
-		// Number of similarities in s as s1
-		
 		String s="entertainment english entity";
-		
 		String s1="en";
 		
 		int count=0;
 		
-		
-		while(s.indexOf(s1)>=0){
-			count++;
+		while(s.indexOf(s1)>=0){	// .indexOf method returns values (>=0) if String present else return (-1)
+			count++;		// if String present increment the value of count
 			System.out.println(s+": "+s.indexOf(s1));
-			s=s.substring(s.indexOf(s1)+s1.length());
+			s=s.substring(s.indexOf(s1)+s1.length()); // exclude the part of string which is searched and matched
 		}
 		
 		System.out.println("count: "+count);
